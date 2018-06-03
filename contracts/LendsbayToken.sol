8f273b1ab638ec97aa169281ce89a5149949933e
Vladimir Gorbunov <vsg@suburban.me>
2018-06-03 23:39:38 +0300

    Add token

---
+++ contracts/LendsbayToken.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+/* Pretty common ERC20 token without any additional logic */
+
+contract LendsbayToken is StandardToken {
+    /* We don't use detailed ERC20 from openzeppelin for simplicity */
+    string public constant name = "Lendsbay Token";
+    string public constant symbol = "LBT";
+    uint8 public constant decimals = 18;
+
+    constructor() public {
+        totalSupply_ = 100 * 1000000 * (10 ** uint256(decimals)); /* 100 mln */
+        /* Add everything to lendsbay account */
+        balances[msg.sender] = totalSupply();
+    }
+}
